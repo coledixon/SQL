99de86fbe0c773c95cc65ba6c5305cf8a2d57dd5
colefromportland <colefromportland@gmail.com>
2018-10-11 12:44:16 -0600

    built proc for check outs
    logic for checking out a book

--- library_database/schema.sql
+++ library_database/schema.sql
@@ -224,6 +224,71 @@ GO
 
 /* --- PROCS ---*/
 -- TO DO
+IF OBJECT_ID('dbo.spCheckOut') is null
+BEGIN
+	CREATE PROC [dbo]. [spCheckOut]
+	@bk_title varchar(50)
+	, @branch_name varchar(25)
+	, @card_no int
+	, @retval int = null OUTPUT
+	, @err_mess varchar(250) = null OUTPUT
+	AS
+	-- handle logic for checking out a book
+	DECLARE @book_id int, @branch_id int, @tran_id int, @no_ofCopies int
+	SET @retval = 0 -- default return value
+
+	SELECT @book_id = book_id 
+		FROM book WHERE bk_title = @bk_title
+
+		IF (COALESCE(@book_id,0)=0)
+		BEGIN
+			SELECT @err_mess = 'ERROR RETREIVING BOOK ID', @retval = -1
+			GOTO ERROR
+		END
+	
+	SELECT @branch_id = branch_id 
+		FROM library_branch WHERE branch_name = @branch_name
+
+		IF (COALESCE(@branch_id,0)=0)
+		BEGIN
+			SELECT @err_mess = 'ERROR RETREIVING BRANCH ID', @retval = -1
+			GOTO ERROR
+		END
+
+	IF NOT EXISTS(SELECT 1 FROM book_copies 
+		WHERE branch_id = @branch_id AND book_id = @book_id)
+	BEGIN
+		INSERT book_copies (book_id, branch_id, no_ofCopies)
+		VALUES (@book_id, @branch_id, 1)
+	END
+
+	SELECT @no_ofCopies = no_ofCopies
+		FROM book_copies WHERE book_id = @book_id AND branch_id = @branch_id
+
+	SELECT @tran_id = tran_id
+		FROM tran_id
+
+	INSERT book_loans (tran_id, book_id, branch_id, card_no, date_due, status)
+	VALUES (@tran_id, @book_id, @branch_id, @card_no, DATEADD(day,5,GETDATE()), 1)
+
+	UPDATE tran_id SET tran_id = (@tran_id + 1) -- increment tran_id
+
+	IF (COALESCE(@no_ofCopies,0) > 0)
+	BEGIN
+		UPDATE book_copies SET no_ofCopies = (@no_ofCopies - 1) -- update copies on-hand
+			WHERE branch_id = @branch_id AND @book_id = @book_id
+	END
+
+	SUCCESS:
+		IF (COALESCE(@retval,0)=0) SET @retval = 1
+		RETURN
+
+	ERROR:
+
+END
+
+GO
+
 -- update books due_date
 -- returned book
 -- checked out book
