d3e513dddd1980935bc567d71f71c5a8a55a65be
colefromportland <colefromportland@gmail.com>
2018-10-11 12:01:41 -0600

    modifications to schema

--- library_database/schema.sql
+++ library_database/schema.sql
@@ -1,6 +1,5 @@
 /* SCHEMA FOR SQL LIBRARY PROJECT */
 
-
 IF NOT EXISTS(SELECT name FROM sys.databases WHERE name ='db_library')
 BEGIN
 	CREATE DATABASE [db_library]
@@ -8,17 +7,22 @@ END
 
 GO
 
-USE db_library
+USE [db_library]
+
 GO
 
+
 /* --- TABLES --- */
+-- TO DO
+-- book_loans to be used as a trans table (only hold active records, not store data)
+
 IF OBJECT_ID('dbo.library_branch') is null
 BEGIN
 	-- library branches
 	CREATE TABLE library_branch (
-			[branch_id] int IDENTITY(0001,1),
+			[branch_id] int IDENTITY(1,1),
 			[branch_name] varchar(25) not null,
-			[branch_address] varchar(30) not null,
+			[branch_address] varchar(100) not null,
 		PRIMARY KEY NONCLUSTERED 
 		(
 			[branch_id] ASC
@@ -32,10 +36,10 @@ IF OBJECT_ID('dbo.borrower') is null
 BEGIN
 	-- borrowers
 	CREATE TABLE borrower (
-			[card_no] int IDENTITY(0001,1),
+			[card_no] int IDENTITY(102,7),
 			[bwr_name] varchar(40) not null,
-			[bwr_address] varchar(30) not null,
-			[bwr_phone] varchar(12) not null,
+			[bwr_address] varchar(100) not null,
+			[bwr_phone] varchar(14) not null,
 		PRIMARY KEY NONCLUSTERED 
 		(
 			[card_no] ASC
@@ -50,14 +54,15 @@ END
 
 GO
 
+
 IF OBJECT_ID('dbo.publisher') is null
 BEGIN
 	-- publishers
 	CREATE TABLE publisher (
-			[pub_id] int IDENTITY(0001,1) not null,
+			[pub_id] int IDENTITY(1,1) not null,
 			[pub_name] varchar(50) not null,
-			[pub_address] varchar(30) null,
-			[pub_phone] varchar(12) null,
+			[pub_address] varchar(100) null,
+			[pub_phone] varchar(14) null,
 		PRIMARY KEY NONCLUSTERED 
 		(
 			[pub_id] ASC
@@ -71,7 +76,7 @@ IF OBJECT_ID('dbo.book') is null
 BEGIN
 	-- book records
 	CREATE TABLE book (
-			[book_id] int IDENTITY(0001,1) not null,
+			[book_id] int IDENTITY(1,10) not null,
 			[bk_title] varchar(50) not null,
 			[pub_id] int not null,
 		CONSTRAINT fk_bkPublisher FOREIGN KEY (pub_id) REFERENCES publisher(pub_id),
@@ -93,7 +98,7 @@ IF OBJECT_ID('dbo.author') is null
 BEGIN
 	-- authors
 	CREATE TABLE author (
-			[book_id] int not null,
+			[book_id] int null,
 			[author_name] varchar(40) not null,
 			[pub_id] int null,
 		CONSTRAINT fk_authPublisher FOREIGN KEY (pub_id) REFERENCES publisher(pub_id),
@@ -107,15 +112,15 @@ IF OBJECT_ID('dbo.book_loans') is null
 BEGIN
 	-- book loans and transactions
 	CREATE TABLE book_loans (
+			[tran_id] int not null,
 			[book_id] int not null,
 			[branch_id] int not null,
 			[card_no] int not null,
-			[date_out] datetime not null,
 			[date_due] datetime not null,
 			[status] int not null,
 		CONSTRAINT fk_loanBook FOREIGN KEY (book_id) REFERENCES book(book_id),
 		CONSTRAINT fk_loanBranch FOREIGN KEY (branch_id) REFERENCES library_branch(branch_id),
-		CONSTRAINT fk_loanBorrower FOREIGN KEY (card_no) REFERENCES borrower(card_no),
+		CONSTRAINT fk_loanBorrower FOREIGN KEY (card_no) REFERENCES borrower(card_no)
 	)
 
 		ALTER TABLE book_loans 
@@ -141,39 +146,87 @@ BEGIN
 			[book_condition] varchar(50) null,
 			[no_ofCopies] int not null,
 		CONSTRAINT fk_copyBook FOREIGN KEY (book_id) REFERENCES book(book_id),
-		CONSTRAINT fk_copyBranch FOREIGN KEY (branch_id) REFERENCES library_branch(branch_id),
+		CONSTRAINT fk_copyBranch FOREIGN KEY (branch_id) REFERENCES library_branch(branch_id)
 	)
 
 		ALTER TABLE book_copies 
 			ADD CONSTRAINT def_copies DEFAULT (0) FOR [no_ofCopies]
+END
 
-		CREATE UNIQUE NONCLUSTERED INDEX natKey_copies ON book_copies(
-			[book_id],
-			[branch_id]
+GO
+
+IF OBJECT_ID('dbo.books_out') is null
+BEGIN
+	-- books checked out
+	CREATE TABLE books_out (
+			[tran_id] int not null,
+			[book_id] int not null,
+			[branch_id] int not null,
+			[date_out] datetime not null,
+		CONSTRAINT fk_outBook FOREIGN KEY (book_id) REFERENCES book(book_id),
+		CONSTRAINT fk_outBranch FOREIGN KEY (branch_id) REFERENCES library_branch(branch_id)
+	)
+		CREATE UNIQUE NONCLUSTERED INDEX natKey_out ON books_out (
+			[tran_id],
+			[book_id]
 		)
 END
 
 GO
 
-/* --- VIEWS --- */
-IF OBJECT_ID('dbo.vbooks_out') is null 
+IF OBJECT_ID('dbo.books_lost') is null
 BEGIN
-	-- all books currently out
-	CREATE VIEW vbooks_out
-	AS
-	SELECT bk_title, date_out, date_due
-		FROM books b
-		JOIN book_loans bl (NOLOCK) ON a.book_id = bl.book_id
-		GROUP BY bk_title, date_out, date_due
-		HAVING date_due > GETDATE()
+	-- relinquished books (past 30 days checked out)
+	CREATE TABLE books_lost (
+			[tran_id] int not null,
+			[card_no] int not null,
+			[branch_id] int not null,
+			[book_id] int not null,
+			[cost] varchar(10) null,
+		CONSTRAINT fk_lostBook FOREIGN KEY (book_id) REFERENCES book(book_id),
+		CONSTRAINT fk_lostBranch FOREIGN KEY (branch_id) REFERENCES library_branch(branch_id),
+		CONSTRAINT fk_lostCard FOREIGN KEY (card_no) REFERENCES borrower(card_no)
+	)
+		CREATE UNIQUE NONCLUSTERED INDEX natKey_lost ON books_lost (
+			[tran_id],
+			[book_id],
+			[card_no]
+		)
+END
 
+GO
+
+IF OBJECT_ID('dbo.tran_id') is null
+BEGIN
+	-- tran master
+	CREATE TABLE tran_id (
+			[tran_id] int not null
+	)
 END
 
 GO
 
 
+/* --- VIEWS --- */
+--IF OBJECT_ID('dbo.vbooks_out') is null 
+--BEGIN
+--	-- all books currently out
+--	CREATE VIEW vbooks_out
+--	AS
+--	SELECT bk_title, date_out, date_due
+--		FROM books b
+--		JOIN book_loans bl (NOLOCK) ON a.book_id = bl.book_id
+--		GROUP BY bk_title, date_out, date_due
+--		HAVING date_due > GETDATE()
+
+--END
+
+--GO
+
+
 /* --- PROCS ---*/
 -- TO DO
--- update books duedate
+-- update books due_date
 -- returned book
 -- checked out book
+-- relinquished book
