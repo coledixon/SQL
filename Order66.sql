2b0b4f5f02f1b61b4fbec13a389a314439f33738
colefromportland <colefromportland@gmail.com>
2019-10-29 15:42:01 -0600

    Update Order66.sql

--- Order66.sql
+++ Order66.sql
@@ -3,13 +3,11 @@ GO
 
 CREATE PROC Order66
 AS
-
-DROP TABLE Jedi_Order
-
+	DROP TABLE Jedi_Order
 GO
 
 IF EXISTS(SELECT 1 FROM Jedi_Order)
-BEGIN
-	EXEC Order66
-END
+	BEGIN
+		EXEC Order66
+	END
 GO
