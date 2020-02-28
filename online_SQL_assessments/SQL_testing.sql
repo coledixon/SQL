5feefade391531dcf83e7f724e0690ded1c5d142
colefromportland <colefromportland@gmail.com>
2020-02-28 08:52:35 -0700

    build query 6 for finding specific data using HAVING

--- online_SQL_assessments/SQL_testing.sql
+++ online_SQL_assessments/SQL_testing.sql
@@ -46,5 +46,42 @@ FROM id_records
 
 
 -- 6
--- 
+/*
+
+	SELECT * FROM users;
+
+		user_id  username
+		1        John Doe         
+		2        Jane Don   
+		3        Alice Jones
+		4        Lisa Romero
+
+	SELECT * FROM training_details;
+
+		user_training_id  user_id  training_id  training_date
+		1                 1        1            "2015-08-02"
+		2                 2        1            "2015-08-03"
+		3                 3        2            "2015-08-02"
+		4                 4        2            "2015-08-04"
+		5	              2        2            "2015-08-03"
+		6                 1        1            "2015-08-02"
+		7                 3        2            "2015-08-04"
+		8                 4        3            "2015-08-03"
+		9                 1        4            "2015-08-03"
+		10                3        1            "2015-08-02"
+		11                4        2            "2015-08-04"
+		12                3        2            "2015-08-02"
+		13                1        1            "2015-08-02"
+		14                4        3            "2015-08-03"
+
+	Write a query to to get the list of users who took the a training lesson more than once in the same day, grouped by user and training lesson, each ordered from the most recent lesson date to oldest date.
+
+*/
+SELECT u.user_id, username, training_id, training_date, COUNT(user_training_id) as count
+FROM users u 
+	JOIN training_details td (NOLOCK) ON u.user_id = td.user_id
+		GROUP BY u.user_id, username, training_id, training_date
+		HAVING COUNT(user_training_id) > 1
+		ORDER BY training_date DESC
+
 
