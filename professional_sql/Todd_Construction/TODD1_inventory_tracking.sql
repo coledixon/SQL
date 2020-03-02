600529bebdad2fe5414845643c9274170eedbf67
colefromportland <colefromportland@gmail.com>
2020-03-02 07:28:22 -0700

    Create TODD1_inventory_tracking.sql

---
+++ professional_sql/Todd_Construction/TODD1_inventory_tracking.sql
@@ -0,0 +1,13 @@
+/* 
+	TODD CONSTRUCTION INVENTORY TRACKING
+		Cole Dixon :: Copyright 2020
+*/
+-- PURPOSE: inventory tracking by status and item category
+-- NOTES: unique tools (T) / quantity tools (Q) / kits (K) / materials (M)
+
+/*
+	-- VIEW FOR
+	-- NEW / PURCHASED
+	-- disposed/retired/discontinued VIEW (T,M,Q,K)	
+		-- status (state?)
+*/
