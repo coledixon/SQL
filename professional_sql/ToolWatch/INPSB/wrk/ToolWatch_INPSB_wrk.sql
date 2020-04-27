8db87a547b08760b58128c4a8e54802ae6aa5e09
colefromportland <colefromportland@gmail.com>
2020-04-27 13:39:39 -0600

    refining the query based on investigation and client specs. (work in progress)

--- professional_sql/ToolWatch/INPSB/wrk/ToolWatch_INPSB_wrk.sql
+++ professional_sql/ToolWatch/INPSB/wrk/ToolWatch_INPSB_wrk.sql
@@ -6,23 +6,11 @@
 	The problem is that the cost code can either be on the transferheader or transferline and that needs to be accounted for in the report which currently it only shows the transferheader cost code.
 */
 
-SELECT DISTINCT Assignment, AssignmentEntityNumber as Job, LastTransferDate, LastTransferNumber, ItemNumber,
-	tb.Description, tb.Quantity, rsh.Description as RateSheet,
-	code.Description as CostCode, cent.RateSheetIdTools, rsl.Type, rsl.MonthlyRate, cent.Description,
-	CASE WHEN rsl.MonthlyRate >0 
-		THEN rsl.MonthlyRate *176 ELSE 0 END AS 'Monthly Rate'
-FROM Toolbrowser tb
-	JOIN Ratesheetlines rsl (NOLOCK) ON tb.ModelId = rsl.ModelId
-	JOIN TransferLines tl (NOLOCK) ON tb.LastTransferLineId = tl.TransferLineId
-	JOIN CostCenters cent (NOLOCK) ON tl.ToCostCenterId = cent.CostCenterId
-	JOIN CostCodes code (NOLOCK) ON tl.ToCostCodeId = code.CostCodeId
-	JOIN BillingLines bl (NOLOCK) ON rsl.RatesheetLineID = bl.RateSheetLineId
-	INNER JOIN RateSheetHeaders rsh (NOLOCK) ON cent.RateSheetIdTools = rsh.RateSheetHeaderId
-	--INNER JOIN RateSheetLines rs (NOLOCK) ON rsh.RateSheetHeaderId = rs.RateSheetHeaderId
-		WHERE Assignment IS NOT NULL and Assignment != 'Yard' and bl.CostCenterId = tl.ToCostCenterId
-		ORDER BY Assignment, ItemNumber
-
-
+-- find columns within schema
+SELECT      COLUMN_NAME AS 'ColumnName', TABLE_NAME AS  'TableName'
+FROM        INFORMATION_SCHEMA.COLUMNS
+WHERE       COLUMN_NAME LIKE '%assignment%' -- replace val with desired col name
+ORDER BY    TableName, ColumnName
 
 --
 ------ WORK -----
@@ -33,6 +21,8 @@ SELECT DISTINCT Assignment, AssignmentEntityNumber as Job, LastTransferDate, Las
 	code.Description as CostCode, cent.RateSheetIdTools, rsl.Type, rsl.MonthlyRate, cent.Description,
 	CASE WHEN rsl.MonthlyRate >0 
 		THEN rsl.MonthlyRate *176 ELSE 0 END AS 'Monthly Rate'
+
+SELECT DISTINCT *
 FROM Toolbrowser tb
 	JOIN Ratesheetlines rsl (NOLOCK) ON tb.ModelId = rsl.ModelId
 	JOIN TransferLines tl (NOLOCK) ON tb.LastTransferLineId = tl.TransferLineId
@@ -45,10 +35,46 @@ FROM Toolbrowser tb
 		ORDER BY Assignment, ItemNumber
 
 
+select top 10 Assignment, *
+	from ToolBrowser
+
+	
+-- NULL ToCostCodeId affecting server performance
+select ToCostCodeId, FromCostCodeId, ToCostCenterId, FromCostCenterId, * from TransferLines
+	where ToCostCenterId IS NULL
+		OR FromCostCodeId IS NULL
+		OR ToCostCenterId IS NULL
+		OR FromCostCenterId IS NULL
+
+
+select top 100 tl.ToCostCodeId as LineToCode, th.CostCodeIDTo as HeadToCode, -- to code 
+	tl.FromCostCodeId as LineFromCode, th.CostCodeIdFrom as HeadFromCode, -- from code
+	tl.ToCostCenterId as LineToCenter, th.CostCenterIdTo as HeadToCenter, -- to cent
+	tl.FromCostCenterId as LineFromCenter, th.CostCenterIdFrom as HeadFromCenter, -- from cent
+	* from TransferHeaders th
+join TransferLines tl (NOLOCK) ON tl.TransferHeaderId = th.TransferHeaderId
+	where ToCostCenterId IS NULL
+		OR FromCostCodeId IS NULL
+		OR ToCostCenterId IS NULL
+		OR FromCostCenterId IS NULL
+
+select * from BillingLines
+where CostCenterId IS NULL
 
 
 
-SELECT * 
-	FROM TransferHeaders th
-	JOIN TransferLines tl (NOLOCK) ON th.TransferHeaderId = tl. TransferHeaderId
-	JOIN 
+/*
+	DEV NOTES
+
+	if TO header has CostCenterIdFrom than TO line has FromCostCenterId
+
+	if TO header has CostCenterIdTo than TO line has ToCostCenterId
+
+	no correlation to TO header or TO line costCode
+
+	COALESCE(th.CostCodeIDTo, tl.ToCostCodeId) -- elim NULLS (line first, than header so reverse logic)
+
+	-- looks like anytime there is a header CostCenterTo, there is atleast a header CostCodeIdTo
+
+*/
+
